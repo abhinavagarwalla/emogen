e71b3235a820ef3441ee188fc4e8e3eb3a0b4b8a
Abhinav Agarwalla <navabhi174@gmail.com>
2015-04-10 22:36:21 +0530

    adding ANN

---
+++ src/detector/ANNEmoDetector.cpp
@@ -0,0 +1,32 @@
+/**
+ *
+ * @file    SVMEmoDetector.cpp
+ * @date    12/27/2013 12:12:58 PM
+ * @brief   Implementation of SVMEmoDetector
+ *
+ * @details
+ *
+ */
+
+#include "ANNEmoDetector.h"
+#include "ANNClassifier.h"
+
+namespace emotime {
+
+  ANNEmoDetector::ANNEmoDetector() : EmoDetector() {
+
+  }
+
+  ANNEmoDetector::ANNEmoDetector(std::map<std::string, std::pair<vector<Emotion>, Classifier*> >
+      detmap_ext) : EmoDetector(detmap_ext) {
+  }
+
+
+  Classifier* ANNEmoDetector::createClassifier() {
+    return new ANNClassifier();
+  }
+
+  std::pair<Emotion, float> ANNEmoDetector::predict(cv::Mat& frame) {
+    return EmoDetector::predictVotingOneVsAllExt(frame);
+  }
+}
---
+++ src/detector/ANNEmoDetector.h
@@ -0,0 +1,65 @@
+/**
+ *
+ * @file    SVMEmoDetector.h
+ * @brief   Defines SVMEmoDetector
+ *
+ */
+
+#ifndef  __ANNEMODETECTOR_INC__
+#define  __ANNEMODETECTOR_INC__
+
+#include "EmoDetector.h"
+
+namespace emotime {
+
+  /**
+   * @class    SVMEmoDetector
+   * @date    12/27/2013 11:59:26 AM
+   *
+   * @brief   EmoDetector specialization using SVMClassifier
+   *
+   */
+  class ANNEmoDetector : public EmoDetector {
+    public:
+
+      /**
+       *  @brief          Creates an SVMEmoDetector with svm parameters and
+       *                  empty classifiers.
+       *
+       *  @param[in]      C_factor  The algorithm C factor
+       *  @param[in]      max_iteration Maximum number of iteration termination criteria
+       *  @param[in]      error_margin Minimum error termination criteria
+       *
+       *  @see SVMClassifier
+       */
+      ANNEmoDetector();
+
+      /**
+       *  @brief          Creates an SVMEmoDetector with svm parameters and
+       *                  classifiers.
+       *
+       *  @param[in]      C_factor  The algorithm C factor
+       *  @param[in]      max_iteration Maximum number of iteration termination criteria
+       *  @param[in]      error_margin Minimum error termination criteria
+       *  @param[in]      detmap_ext Mapping between emotions and classifier.
+       *
+       *  @see SVMClassifier
+       */
+      ANNEmoDetector(std::map<std::string, std::pair<vector<Emotion>, Classifier*> >
+          detmap_ext);
+
+      std::pair<Emotion, float> predict(cv::Mat& frame);
+
+    protected:
+
+      Classifier* createClassifier();
+
+    private:
+
+
+  }; /* -----  end of class SVMEmoDetector  ----- */
+
+}
+
+
+#endif   /* ----- #ifndef __SVMEMODETECTOR_INC__  ----- */
